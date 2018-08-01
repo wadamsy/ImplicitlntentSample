a1fef7b647fb14eed4cf5a9a52fbc7600b2ac0d5
wadamasaya <16010418@anabuki-college.ac.jp>
2018-08-01 10:33:52 +0900

    init

---
+++ app/src/main/java/wadamasaya/implicitlntentsample/IntentStartActivity.java
@@ -0,0 +1,13 @@
+package wadamasaya.implicitlntentsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntentStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_start);
+    }
+}
