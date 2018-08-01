2b72bd9a8d8e57c9d43ccff204f707bf35549528
wadamasaya <16010418@anabuki-college.ac.jp>
2018-08-01 11:38:06 +0900

    test

--- app/src/main/java/wadamasaya/implicitlntentsample/IntentStartActivity.java
+++ app/src/main/java/wadamasaya/implicitlntentsample/IntentStartActivity.java
@@ -1,13 +1,105 @@
 package wadamasaya.implicitlntentsample;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class IntentStartActivity extends AppCompatActivity {
+
+    private TextView _tvLatitude;
+
+    private TextView _tvLongitude;
+
+    private double _latitude = 0;
+
+    private double _longitude = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_start);
+
+        _tvLatitude = findViewById(R.id.tvLatitude);
+        _tvLongitude = findViewById(R.id.tvLongitude);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        GPSLocationListener locationListener = new GPSLocationListener();
+        if (ActivityCompat.checkSelfPermission(IntentStartActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            String [] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(IntentStartActivity.this,permissions,1000);
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
+    public  void onMapSearchButtonClick(View view){
+        EditText etSearchWord = findViewById(R.id.etSearchWord);
+        String searchWord = etSearchWord.getText().toString();
+
+        try {
+            searchWord = URLEncoder.encode(searchWord, "UTF-8");
+            String uriStr = "geo:0,0?q=" + searchWord;
+            Uri uri = Uri.parse(uriStr);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+        catch (UnsupportedEncodingException ex){
+            Log.e("IntentStartActivity","検索キーワード変換失敗",ex);
+        }
+    }
+    public void onMapShowCurrentButtonClick(View view) {
+        String uriStr = "geo:" + _latitude + "," + _longitude;
+        Uri uri = Uri.parse(uriStr);
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+    private class GPSLocationListener implements LocationListener{
+
+        @Override
+        public void onLocationChanged(Location location){
+            _latitude = location.getLatitude();
+            _longitude = location.getLongitude();
+            _tvLatitude.setText(Double.toString(_latitude));
+            _tvLongitude.setText(Double.toString(_longitude));
+        }
+        @Override
+        public void onStatusChanged(String provider,int status,Bundle extras){}
+
+        @Override
+        public void onProviderEnabled(String provider){}
+
+        @Override
+        public void onProviderDisabled(String provider){}
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String [] permissions,int[] grantResults){
+        if (requestCode  == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            GPSLocationListener locationListener = new GPSLocationListener();
+            if (ActivityCompat.checkSelfPermission(IntentStartActivity.this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+        }
+    }
+
 }
